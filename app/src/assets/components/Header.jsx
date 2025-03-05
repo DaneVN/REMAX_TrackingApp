f6bb10eaac6a423ce3619b0a318b76582c69304c
DaneVN <Dane.vN23@gmail.com>
2025-03-05 14:12:18 +0200

    Images path bugfix

--- app/src/assets/components/Header.jsx
+++ app/src/assets/components/Header.jsx
@@ -5,15 +5,16 @@ export default function Header() {
     <header
       id="home"
       className="flex justify-between items-center p-2.5 text-white 
-      relative z-10 bg-auto bg-[url('./src/assets/images/REMAXBanner.jpg')] 
+      relative z-10 bg-auto bg-[url('/images/REMAXBanner.jpg')] 
       bg-[50%]"
     >
       <img
-        src="../src/assets/images/REMAXUnity.png"
+        src="/images/REMAXUnity.png"
+        // src="https://drive.google.com/file/d/1QwCMNWikJNdsyRxu_LZl8p3vZEbIr0Hi/view?usp=drive_link"
         alt="RE/MAX Unity Logo"
         className="max-h-[20vh] object-contain max-w-2/3"
       />
-      <span className="text-2xl font-bold text-[var(--cl-5)]">
+      <span className="text-2xl sm:text-4xl font-bold text-[var(--cl-5)]">
         Tracking App
       </span>
     </header>
