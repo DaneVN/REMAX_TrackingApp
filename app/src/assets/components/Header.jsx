25c9eda9fdec5de12d297231c98b9c71efa6874a
DaneVN <Dane.vN23@gmail.com>
2025-03-05 11:51:53 +0200

    Header BG + navbar style fix

--- app/src/assets/components/Header.jsx
+++ app/src/assets/components/Header.jsx
@@ -4,18 +4,18 @@ export default function Header() {
   return (
     <header
       id="home"
-      className="flex justify-between items-start p-2.5 text-white 
-      relative z-10 bg-cover bg-[url('./src/assets/images/REMAXBanner.jpg')] 
-      bg-[position:18%_20%]"
+      className="flex justify-between items-center p-2.5 text-white 
+      relative z-10 bg-auto bg-[url('./src/assets/images/REMAXBanner.jpg')] 
+      bg-[50%]"
     >
-      <span className="text-2xl font-bold text-[var(--cl-5)]">
-        Tracking App
-      </span>
       <img
         src="../src/assets/images/REMAXUnity.png"
         alt="RE/MAX Unity Logo"
         className="max-h-[20vh] object-contain max-w-2/3"
       />
+      <span className="text-2xl font-bold text-[var(--cl-5)]">
+        Tracking App
+      </span>
     </header>
   );
 }
--- app/src/assets/components/NavBar.jsx
+++ app/src/assets/components/NavBar.jsx
@@ -3,20 +3,29 @@ import React from "react";
 export default function NavBar() {
   return (
     <nav
-      className="bg-[var(--cl-2)] flex justify-around p-1.25 text-[var(--cl-4)] 
+      className="bg-[var(--cl-2)] flex justify-around px-1.25 pt-1.25 text-[var(--cl-4)] 
     border-y-2 border-[var(--cl-1)] sticky top-0 z-20 transition-top duration-300 
     ease-in-out"
     >
-      <div className="px-1.25">
-        <p>0</p>
+      <div className="px-1.25 bg-[var(--cl-6)] rounded-full w-12 h-12">
+        {/* profile picture */}
       </div>
-      <a href="#home" className="px-2.5 text-[var(--cl-4)] no-underline">
+      <a
+        href="#home"
+        className="text-center hover:bg-[var(--cl-6)] rounded-t-3xl bg-[var(--cl-5)] text-[var(--cl-2)] w-1/6 py-1 px-2.5 no-underline"
+      >
         Home
       </a>
-      <a href="#stats" className="px-2.5 text-[var(--cl-4)] no-underline">
+      <a
+        href="#stats"
+        className="text-center hover:bg-[var(--cl-6)] rounded-t-3xl bg-[var(--cl-5)] text-[var(--cl-2)] w-1/6 py-1 px-2.5 no-underline"
+      >
         Stats
       </a>
-      <a href="#sheet" className="px-2.5 text-[var(--cl-4)] no-underline">
+      <a
+        href="#sheet"
+        className="text-center hover:bg-[var(--cl-6)] rounded-t-3xl bg-[var(--cl-5)] text-[var(--cl-2)] w-1/6 py-1 px-2.5 no-underline"
+      >
         Activity Sheet
       </a>
     </nav>
