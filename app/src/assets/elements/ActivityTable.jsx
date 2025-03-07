735ad6f7232f9c57f3bbae4e0e631cfee4fde539
DaneVN <Dane.vN23@gmail.com>
2025-03-07 11:31:19 +0200

    Table style bugfix

--- app/src/assets/elements/ActivityTable.jsx
+++ app/src/assets/elements/ActivityTable.jsx
@@ -16,18 +16,18 @@ const ActivityTable = () => {
   const dates = Array.from({ length: daysInMonth }, (_, i) => i + 1);
 
   return (
-    <div className="p-4 bg-[var(cl-1)] rounded-[15px] overflow-auto max-h-[60vh] w-full">
+    <div className="py-4 rounded-[15px] overflow-auto max-h-[60vh] w-full">
       <table className="w-max min-w-full border-collapse">
         {/* Header Row with Dates */}
         <thead>
-          <tr className="bg-[var(cl-4)] text-[var(cl-2)]">
-            <th className="p-2 border border-[var(cl-2)] sticky top-0 bg-[var(cl-4)]">
+          <tr className="bg-[var(--cl-4)] text-[var(--cl-2)] text-[12px]">
+            <th className="text-[12px] p-2 border border-[var(--cl-2)] sticky top-0 bg-[var(--cl-4)]">
               Activity
             </th>
             {dates.map((date) => (
               <th
                 key={date}
-                className="p-2 border border-[var(cl-2)] sticky top-0 bg-[var(cl-4)]"
+                className="text-[12px] p-2 border border-[var(--cl-2)] sticky top-0 bg-[var(--cl-4)]"
               >
                 {date}
               </th>
@@ -37,12 +37,12 @@ const ActivityTable = () => {
         {/* Body Rows with Activities */}
         <tbody>
           {activities.map((activity, index) => (
-            <tr key={index} className="hover:bg-[var(cl-5)]">
-              <td className="p-2 border border-[var(cl-2)] font-bold">{`${activity.name}`}</td>
+            <tr key={index} className="hover:bg-[var(--cl-5)]">
+              <td className="text-[12px] p-2 border border-[var(--cl-2)] font-bold">{`${activity.name}`}</td>
               {dates.map((date) => (
                 <td
                   key={`${activity.name}-${date}`}
-                  className="p-2 border border-[var(cl-2)]"
+                  className="text-[12px] p-2 border border-[var(--cl-2)]"
                 >
                   {/* Placeholder for data (e.g., completed count) */}0
                 </td>
