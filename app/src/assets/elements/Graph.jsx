9f767bd33e889aebdda4fdae2f8a9c154393fcf3
DaneVN <Dane.vN23@gmail.com>
2025-03-13 14:39:10 +0200

    Netilfy bugFix Attempt 2

--- app/src/assets/elements/Graph.jsx
+++ app/src/assets/elements/Graph.jsx
@@ -10,42 +10,74 @@ const Graph = ({ data }) => {
     dataCurr[1].push(activity.name);
   });
 
-  return (
-    <Plot
-      data={[
-        {
-          x: [...dataCurr[0]],
-          y: [...dataCurr[1]],
-          type: "bar",
-          marker: { color: "#003dc5" },
-          orientation: "h",
-          name: "current month",
-        },
-        {
-          x: [...dataPrev[0]],
-          y: [...dataPrev[1]],
-          type: "bar",
-          marker: { color: "#8b7700" },
-          orientation: "h",
-          name: "previous month",
-        },
-      ]}
-      layout={{
-        title: { text: "Comparison" },
-        bgcolor: "#007dc3",
-        paper_bgcolor: "#007dc3",
-        showlegend: false,
-        font: {
-          family: "Courier New, monospace",
-          size: 11,
-          color: "white",
-        },
-      }}
-      config={{ displayModeBar: false }}
-      style={{ width: "100%", height: "100%" }}
-      useResizeHandler={true}
-    />
-  );
+  if (dataPrev[0].length) {
+    return (
+      <Plot
+        data={[
+          {
+            x: [...dataCurr[0]],
+            y: [...dataCurr[1]],
+            type: "bar",
+            marker: { color: "#003dc5" },
+            orientation: "h",
+            name: "current month",
+          },
+
+          {
+            x: [...dataPrev[0]],
+            y: [...dataPrev[1]],
+            type: "bar",
+            marker: { color: "#8b7700" },
+            orientation: "h",
+            name: "previous month",
+          },
+        ]}
+        layout={{
+          title: { text: "Comparison" },
+          bgcolor: "#007dc3",
+          paper_bgcolor: "#007dc3",
+          showlegend: false,
+          font: {
+            family: "Courier New, monospace",
+            size: 11,
+            color: "white",
+          },
+        }}
+        config={{ displayModeBar: false }}
+        style={{ width: "100%", height: "100%" }}
+        useResizeHandler={true}
+      />
+    );
+  } else {
+    return (
+      <Plot
+        data={[
+          {
+            x: [...dataCurr[0]],
+            y: [...dataCurr[1]],
+            type: "bar",
+            marker: { color: "#003dc5" },
+            orientation: "h",
+            name: "current month",
+          },
+        ]}
+        layout={{
+          title: { text: "Comparison" },
+          bgcolor: "#007dc3",
+          paper_bgcolor: "#007dc3",
+          showlegend: false,
+          font: {
+            family: "Courier New, monospace",
+            size: 11,
+            color: "white",
+          },
+        }}
+        config={{ displayModeBar: false }}
+        style={{ width: "100%", height: "100%" }}
+        useResizeHandler={true}
+      />
+    );
+  }
 };
 
 export default Graph;
